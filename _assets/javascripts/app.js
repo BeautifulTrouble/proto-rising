b3ac7d331d0fff47c51b4d37a0b6d56327eee820
Adrian Carpenter <adriatic.c@gmail.com>
2015-05-19 00:47:21 -0500

    Add basic templates and rough blocking with sessionStorage app state

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -5,8 +5,58 @@
 
 //= require backbone-app.js
 
+// Start
+// ===================================================================
 $(function() {
-    App.main();
     console.log('Running...');
+    App.main();
 });
+
+
+// Utilities
+// ===================================================================
+var JSONStorage = function(store, prefix) {
+    // Local = JSONStorage(typeof localStorage !== "undefined" ? localStorage : {});
+    // Local('x');                                  /* get */
+    // Local('x', [1,2,3,4]);                       /* set */
+    // Local('x', null);                            /* delete */
+    // Local('x', possiblyUndefined, [1,2,3,4]);    /* set w/smart default */
+    prefix = prefix || '';
+    return function (key, value, fallbackValue) {
+        // With a fallbackValue, you can be lazy about checking whether it
+        // makes sense to store a default fallback value. When a fallbackValue
+        // is specified but the given value is undefined, the fallbackValue is
+        // stored in the JSONStorage object. This is true UNLESS a stored value 
+        // already exists, which will always take precedence over a fallback.
+        //
+        // This can be thought of as an update-if-given-better-information 
+        // operation.
+        //
+        //  someRoute: function(username) {
+        //      /* Notice no argument checking is needed */
+        //      StorageObject('username', username, 'anonymous');
+        //      ...
+        //
+        if (value == null) {
+            var existingValue = JSON.parse(store[prefix + key] || 'null');
+            if (value === void 0 && fallbackValue === void 0) {
+                return existingValue;                   // get
+            } else if (fallbackValue !== void 0) {
+                value = existingValue || fallbackValue; // set w/smart default
+            }
+        }
+        store[prefix + key] = JSON.stringify(value);    // set or delete
+        return value;
+    };
+};
+
+
+// "Footer scripts"
+// ===================================================================
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+e.src='https://www.google-analytics.com/analytics.js';
+r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+ga('create','UA-XXXXX-X','auto');ga('send','pageview');
 
--- _assets/javascripts/backbone-app.js
+++ _assets/javascripts/backbone-app.js
@@ -3,6 +3,8 @@
 //= require backbone.stickit.js
 //= require backbone-forms.js
 
+//= require header
+//= require footer
 //= require module-list
 //= require module-detail
 
@@ -11,30 +13,86 @@
 window.App = {};
 
 App.main = function() {
+    // Use sessionStorage for state because most of this state should
+    // be specific to the session and not long-term persisted.
+    App.state = JSONStorage(typeof sessionStorage !== 'undefined' ? sessionStorage : {}, 'state-');
     App.router = new App.Router();
     Backbone.history.start({
         pushState: false // TODO
     });
 };
 
+
 // Models
 // ===================================================================
-
-// Views
-// ===================================================================
-App.ModuleListView = Backbone.View.extend({
-    template: "module-list",
-    initialize: function(options) {
-    },
-    serialize: function() {
-        return {
-        };
-    },
-    events: {
+App.Module = Backbone.Model.extend({
+    initialize: function() {
     }
 });
 
-App.ModuleDetailView = Backbone.View.extend({
+
+// Views
+// ===================================================================
+App.View = Backbone.View.extend({
+    // Extend the standard view with useful things
+    constructor: function() {
+        // Add some common event handlers to all views
+        this.events = _.extend({
+            // Handle <button data-route="about/authors">Authors</button>
+            'click button': function(ev) {
+                var route = ev.target.dataset.route;
+                route && App.router.navigate(route, {trigger: true});
+            }
+        }, this.events || {});
+
+        // Button group handling
+        this.groupSelect = function(groupSelector, selectedClass, activeElement) {
+            // groupSelector: selector that will capture each item in the group
+            // selectedClass: class(es) which the selected item should have
+            // activeElement: selector or $el to be assigned selected class(es)
+            this.$(groupSelector).removeClass(selectedClass); 
+            this.$(activeElement).addClass(selectedClass);
+        };
+
+        // Get element with matching data attribute
+        this.dataEl = function(key, value) {
+            return this.$('[data-' + key + '="' + value + '"]')
+        };
+
+        Backbone.View.apply(this, arguments);
+    }
+});
+
+App.HeaderView = App.View.extend({
+    template: "header",
+});
+
+App.FooterView = App.View.extend({
+    template: "footer",
+});
+
+App.ModuleListView = App.View.extend({
+    template: "module-list",
+    afterRender: function() {
+        var category = App.state('category');
+        var sorting = App.state('sorting');
+        this.groupSelect('.category', 'selected btn-info', this.dataEl('category', category));
+        this.groupSelect('.sorting', 'selected', this.dataEl('sorting', sorting));
+        App.router.navigate('category/' + category + '/' + sorting);    // Not triggering the route
+    },
+    events: {
+        'click .category': function(ev) {
+            App.state('category', ev.target.dataset.category);
+            this.render();
+        },
+        'click .sorting': function(ev) {
+            App.state('sorting', ev.target.dataset.sorting);
+            this.render();
+        }
+    }
+});
+
+App.ModuleDetailView = App.View.extend({
     template: "module-detail",
     initialize: function(options) {
     },
@@ -46,11 +104,11 @@ App.ModuleDetailView = Backbone.View.extend({
     }
 });
 
+
 // Layouts
 // ===================================================================
 Backbone.Layout.configure({
     manage: true,
-
     // Templates should be embedded (require them at top of this file)
     fetchTemplate: function(path) {
         var JST = window.JST || {};
@@ -58,12 +116,12 @@ Backbone.Layout.configure({
             return JST[path];
         }
     },
-
     // Shortcut for instantiating and rendering views
     renderViewEl: '#content',
     renderView: function(view, options) {
         view = new view(options || {});
         this.setView(this.renderViewEl, view);
+        // TODO: Allow passing a mapping of nested views
         this.render();
     }
 });
@@ -71,23 +129,30 @@ Backbone.Layout.configure({
 App.layout = new Backbone.Layout({
     el: 'body',
     views: {
+        '#header': new App.HeaderView(),
+        '#footer': new App.FooterView()
     }
 });
+
 
 // Routes
 // ===================================================================
 App.Router = Backbone.Router.extend({
-    initialize: function() {
-    },
     routes: {
-        'module/:name': 'displayModule',
+        'module/:slug': 'displayModule',
+        'category/:category(/:sorting)': 'displayList',
         '*default': 'default'
     },
-    displayModule: function(name) {
+    displayModule: function(slug) {
         App.layout.renderView(App.ModuleDetailView);
     },
-    'default': function() {
+    displayList: function(category, sorting) {
+        // Set these from the route or saved state with default fallback
+        App.state('category', category, 'all');
+        App.state('sorting', sorting, 'alpha');
         App.layout.renderView(App.ModuleListView);
-    }
+    },
+    'default': function() { this.displayList(); }
 });
+
 
